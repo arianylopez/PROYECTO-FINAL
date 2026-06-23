9ae89786fcff8c1e7669640cd731198916842e48
Ariany Lopez <arianylopez341@gmail.com>
2026-06-23 00:08:36 -0400

    fix: agregando variables en settings

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: int
     ENVIRONMENT: str
     SECRET_KEY: str
     ALGORITHM: str
